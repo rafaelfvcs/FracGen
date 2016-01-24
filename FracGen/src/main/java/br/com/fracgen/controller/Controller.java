93cde78637fdd0cbaa5f8acd4ba720db32047c01
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-01-24 18:52:55 -0300

    added python packages, some classes and dao.properties file

--- FracGen/src/main/java/br/com/fracgen/controller/Controller.java
+++ FracGen/src/main/java/br/com/fracgen/controller/Controller.java
@@ -1,5 +1,9 @@
 package br.com.fracgen.controller;
 
+import javafx.fxml.FXML;
+
 public class Controller {
+
+	@FXML public void exit() {}
 
 }
---
+++ FracGen/src/main/java/br/com/fracgen/util/OpenScanilneData.java
@@ -0,0 +1,5 @@
+package br.com.fracgen.util;
+
+public class OpenScanilneData {
+
+}
