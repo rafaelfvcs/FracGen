bc5c7485724339a06a824fb4a3b6df2819e23799
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-01-23 23:23:52 -0300

    "Added the package to the graphical interface"

---
+++ FracGen/src/main/java/br/com/fracgen/javafxapplication/JavaFXFracGenApplication.java
@@ -0,0 +1,27 @@
+package br.com.fracgen.javafxapplication;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+
+public class JavaFXFracGenApplication extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		primaryStage.setTitle("NFracGen");
+
+		Pane root = FXMLLoader.load(getClass().getResource("/LayoutMain.fxml"));
+
+		Scene scene = new Scene(root,700,500); // 600, 400
+		primaryStage.setScene(scene);
+
+		primaryStage.show();
+	}
+
+}
