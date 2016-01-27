86036b78df6e6c230dc7fa718eaa88ac9b0d648f
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-01-26 23:01:16 -0300

    Added class with task in model package

---
+++ FracGen/src/main/java/br/com/fracgen/model/MainApplication.java
@@ -0,0 +1,10 @@
+package br.com.fracgen.model;
+
+public class MainApplication {
+
+	//TODO: Faz parte do MVC: Aqui deve ficar toda a logica da aplicacao
+	/*
+	 * As aplicacao irá chamar classes dos metodos criados permitindo assim a integracao
+	 * das funcionalidades do programa
+	 */
+}
