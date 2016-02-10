9d8d1eefc0a3cf3341ac89e6c705b3e618fae9e6
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-02-10 10:34:00 -0300

    Revert "Added stage for open data and some utils"

--- FracGen/src/main/java/br/com/fracgen/util/OpenScanilneData.java
+++ FracGen/src/main/java/br/com/fracgen/util/OpenScanilneData.java
@@ -1,36 +1,5 @@
 package br.com.fracgen.util;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 public class OpenScanilneData {
 
-    public List<Double> openCSVFileToDouble(String fileName, String separator,
-            int column) {
-        BufferedReader br = null;
-        List<Double> values = new ArrayList<>();
-        String line = null;
-        try {
-            br = new BufferedReader(new FileReader(fileName));
-            while ((line = br.readLine()) != null) {
-                String[] lineValues = line.split(separator);
-                values.add(Double.valueOf(lineValues[column]));
-            }
-
-        } catch (FileNotFoundException e) {
-        } catch (IOException e) {
-        } finally {
-            if (br != null) {
-                try {
-                    br.close();
-                } catch (IOException e) {
-                }
-            }
-        }
-        return values;
-    }
 }
