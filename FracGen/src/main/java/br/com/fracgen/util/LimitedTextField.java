a008c99e3dcb8c85e2680e2792446aa73ed13191
elidioxg <elidioxg@gmail.com>
2016-02-12 23:20:09 -0300

    LimitedTextFile modified.

--- FracGen/src/main/java/br/com/fracgen/util/LimitedTextField.java
+++ FracGen/src/main/java/br/com/fracgen/util/LimitedTextField.java
@@ -7,4 +7,36 @@ public class LimitedTextField {
     public String[] headerValues;
     public boolean hasHeader = true;
 
+    public void setFilename(String filename) {
+        this.fileName = filename;
+    }
+
+    public String getFilename() {
+        return this.fileName;
+    }
+
+    public void setSeparator(String sep) {
+        this.Separator = sep;
+    }
+
+    public String getSeparator() {
+        return this.Separator;
+    }
+
+    public void setHeaderValues(String[] strings) {
+        this.headerValues = strings;
+    }
+
+    public String[] getHeaderValues() {
+        return this.headerValues;
+    }
+
+    public void setHeader(boolean header) {
+        this.hasHeader = header;
+    }
+
+    public boolean getHeader() {
+        return this.hasHeader;
+    }
+
 }
