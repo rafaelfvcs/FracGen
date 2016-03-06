e6273fb85b9ddcf355459213230f4f3b96b38a57
Rafael F.V.C. Santos <rafaelfvcs@gmail.com>
2016-03-06 15:35:48 -0300

    To connect and save data in the database

--- FracGen/src/main/java/br/com/fracgen/dao/db/ConnectionFactory.java
+++ FracGen/src/main/java/br/com/fracgen/dao/db/ConnectionFactory.java
@@ -1,5 +1,23 @@
 package br.com.fracgen.dao.db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import br.com.fracgen.dao.DAOProperties;
+
 public class ConnectionFactory {
 
+	/**
+	 * Abre uma nova conexão com o banco de dados
+	 * @return Nova conexão
+	 * @throws SQLException
+	 */
+	public static Connection openConnection() throws SQLException {
+		// Cria a URL de conexão com base nas configurações do arquivo
+		String url = String.format("jdbc:mysql://%s:%d/%s", DAOProperties.getDBDAOServerName(), DAOProperties.getDBDAOPort(), DAOProperties.getDBDAODBName());
+
+		// Cria uma nova conexão e retorna
+		return DriverManager.getConnection (url, DAOProperties.getDBDAOUserName(), DAOProperties.getDBDAOPassword());
+	}
 }
